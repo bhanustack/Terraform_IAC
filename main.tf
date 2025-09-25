7b1f55e150ecf6930f2bcb0af6b77c884473d128
bhanustack <98820578+bhanustack@users.noreply.github.com>
2025-09-24 20:53:46 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "example1" {
+    ami =  "ami-0cfde0ea8edd312d4"
+    instance_type = "t3.micro"
+  
+tags = {
+  Name = "terraform-example4"
+  BU = "Energy2"
+  FINID = "1431"
+  Manager = "Sunil"
+}
+}
