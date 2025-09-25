19f131cc5b2e6a09d1e5c5b6b328956e5531cb1c
bhanu <cloudly.bhanu@gmail.com>
2025-09-24 21:08:24 -0500

    Adding user data

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 resource "aws_instance" "example1" {
     ami =  "ami-0cfde0ea8edd312d4"
     instance_type = "t3.micro"
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
   
 tags = {
   Name = "terraform-example4"
-  BU = "Energy2"
-  FINID = "1431"
-  Manager = "Sunil"
-  Patch = "no"
-  Function= "App"
+  
 }
 }
